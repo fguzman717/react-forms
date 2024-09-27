6b34d42323561d5f8254063161d030deb06b70b1
fguzman717 <felixguzman717@gmail.com>
2024-09-26 19:10:32 -0500

    Completed SignUp Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 
 function App() {
+  const [token, setToken] = useState(null);
   return (
     <>
-      <div></div>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
     </>
   );
 }
